9d1cfdc1def123d845ea39f958c77a8cf902c7d4
Nicolas Biancaniello <nicolasbiancaniello@gmail.com>
2024-11-20 18:37:54 -0500

    removed from gitignore image folder

--- front/src/components/api/api.jsx
+++ front/src/components/api/api.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const api = axios.create({
    baseURL: 'https://fs-project-cr99.onrender.com/api',
+   // baseURL: 'https://localhost:5000/api',
 });
 
 export const imgLocation = 'https://fs-project-cr99.onrender.com/static/uploads/';
+// export const imgLocation = 'https://localst:5000/static/uploads/';
